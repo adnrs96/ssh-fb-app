6dc51414ede0079144e55f5f727a395d35328087
Aditya Bansal <adi.bansal241996@gmail.com>
2019-06-24 21:08:44 +0530

    models.py: Add full_name and profile_picture fields to the UserProfile model.

--- server/models.py
+++ server/models.py
@@ -1,10 +1,23 @@
 from django.contrib.auth.models import AbstractBaseUser, UserManager
 from django.db import models
+from django.core.files import File
+import urllib
+import os
 
 # Create your models here.
 class UserProfile(AbstractBaseUser):
     USERNAME_FIELD = 'user_id'
     user_id = models.CharField(unique=True, max_length=32)
+    full_name = models.CharField(max_length=100)
+    profile_picture = models.ImageField(upload_to='profile_pics')
     access_token = models.CharField(max_length=200)
     is_active = models.BooleanField()
     objects = UserManager()
+
+    def update_profile_picture_from_url(self, url):
+        """Store image locally if we have a URL"""
+        result = urllib.request.urlretrieve(url)
+        self.profile_picture.save(
+            os.path.basename(url),
+            File(open(result[0], 'rb'))
+        )
