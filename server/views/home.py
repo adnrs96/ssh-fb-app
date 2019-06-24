1c3b7146dc3cb0876953d6cc385ab291d1a4541c
Aditya Bansal <adi.bansal241996@gmail.com>
2019-06-24 21:10:31 +0530

    home: Add new home template which displays name and profile picture when logged in.

--- server/views/home.py
+++ server/views/home.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+def render_real_home(request: HttpRequest) -> HttpResponse:
+    ctx = {
+        "name": request.user.full_name,
+        "image_url": request.user.profile_picture.url
+    }
+    return render(request, 'home.html', ctx)
+    return HttpResponse('Yeah you are logged in!')
+
 def handle_home(request: HttpRequest) -> HttpResponse:
-    return render(request, 'login.html')
+    if not request.user.is_authenticated:
+        return render(request, 'login.html')
+    return render_real_home(request)
